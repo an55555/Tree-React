8d49394196ceb2916c374d296044e2e74549225c
an55555 <156081289@qq.com>
2017-10-02 02:37:42 +0800

    add tree demo

--- my-app/src/tree2.jsx
+++ my-app/src/tree2.jsx
@@ -1,3 +1,4 @@
+/*分了三个组件 */
 import React, { Component } from 'react';
 import  './com-style/slide.css';
 import './App.css';
@@ -61,8 +62,6 @@ class TreeElement extends React.Component {
             modelList:props.model,
             toSonCheckBoxStatus:props.sendCheckStatus?props.sendCheckStatus:0
         }
-        this.commitCheckState=this.commitCheckState.bind(this)
-
     }
     componentWillReceiveProps(nextProps){
 
@@ -71,17 +70,13 @@ class TreeElement extends React.Component {
             toSonCheckBoxStatus:nextProps.sendCheckStatus
         }));
     }
-    commitCheckState(){return}
-    componentWillUnmount(){
-
-    }
     handleFahrenheitChange(temperature) {
     }
     render(){
         if(this.state.modelList.length){
-            const listItems = this.state.modelList.map((number,index) =>
+            const listItems = this.state.modelList.map((number) =>
                 <ul className="slide-tree" key={number.projectId}>{this.state.toSonCheckBoxStatus}
-                    <TreeItemElement model={number} index={index}  commitCheckState = {this.commitCheckState.bind(this)}/>
+                    <TreeItemElement model={number} sendCheckStatus={this.state.toSonCheckBoxStatus} />
                 </ul>
             );
             return (
@@ -101,75 +96,35 @@ class TreeItemElement extends React.Component{
         super(props);
         this.state={
             modelItem:props.model,
-            modelIndex:props.index,
             modelChild:[],
             special:false,
             isFolder:props.model.hasChild==1,   //控制图标显示，是否有儿子图标
             open:props.model.special=='all'?true:false,  //是否展开
-            checkBoxStatus:props.model.checkState,
+            checkBoxStatus:0,
             toSonCheckBoxStatus:0, //向下传复选框
             sendCheckStatus:props.sendCheckStatus
         }
         this.branchToggle = this.branchToggle.bind(this,false);
         this.getSonData=this.getSonData.bind(this)
         this.checkBoxStatusFn=this.checkBoxStatusFn.bind(this)
-        this.commitCheckState=this.commitCheckState.bind(this)
     }
     componentWillReceiveProps(nextProps){
+        console.log(nextProps)
         this.setState({
-            modelItem:nextProps.model,
-            checkBoxStatus:nextProps.model.checkState,
+            checkBoxStatus:nextProps.sendCheckStatus,
+            sendCheckStatus:nextProps.sendCheckStatus
+            // open:nextProps.open,
+            // isFolder:nextProps.model.hasChild==1
         })
-        this.sentCheckBoxStatusFn(nextProps.model.checkState)
     }
     handleFahrenheitChange(temperature) {
-
     }
-    /*改变儿子CheckBox*/
-    sentCheckBoxStatusFn(state){
-        var modelChild=JSON.parse(JSON.stringify(this.state.modelChild))
-        for(var i in modelChild){
-            modelChild[i]['checkState']=state
-        }
-        this.setState(nexState => ({
-            modelChild:modelChild
-        }));
-    }
-    /*点击改变当前CheckBox*/
     checkBoxStatusFn(){
-        var changeStatus=this.state.checkBoxStatus==2?0:2
         this.setState(nexState => ({
-            checkBoxStatus:changeStatus,
-        }));
-        this.sentCheckBoxStatusFn(changeStatus)
-        /*向上传播*/
-        this.props.commitCheckState(changeStatus,this.state.modelIndex)
-    }
-    commitCheckState(state,index){
-        console.log("儿子的应化"+state+"索引"+index)
-        var modelChild=JSON.parse(JSON.stringify(this.state.modelChild))
-        modelChild[index]['checkState']=state
-        var nowBoll=0
-        var nowState=0;
-        for(var a in modelChild){
-            nowBoll+=modelChild[a]['checkState']
-        }
-        if(nowBoll==0){
-            nowState=0
-        }else if(nowBoll==modelChild.length*2){
-            nowState=2
-        }else{
-            nowState=1
-        }
-        console.log(nowBoll)
-        console.log(modelChild.length*2)
-        console.log(nowState)
-        this.setState(nexState => ({
-            modelChild:modelChild,
-            checkBoxStatus:nowState
+            checkBoxStatus:this.state.checkBoxStatus==2?0:2,
+            toSonCheckBoxStatus:this.state.checkBoxStatus==2?0:2
         }));
     }
-    /*展开儿子*/
     branchToggle(reload){
         if(this.state.special=='all') return
         if(!this.state.isFolder) return
@@ -182,41 +137,27 @@ class TreeItemElement extends React.Component{
             })
         }
     }
-    /*获取儿子*/
     getSonData(){
-        var modelChild=$getTreeList()
-        for(var i in modelChild){
-            modelChild[i]['checkState']=0
-        }
         this.setState({
-            modelChild:modelChild
+            modelChild: $getTreeList()
         })
     }
     render(){
-        var _this=this
      return (
          <li className="slideModel" >
              <div className="slideItem">
                  <div className="slideName ellipsis">
                     <span className="slide-tree-editBox">
-                        <a onClick={this.checkBoxStatusFn} className={"slide-tree-checkbox checkBoxBg checkBoxBg_"+this.state.checkBoxStatus}></a>
+                        <a onClick={this.checkBoxStatusFn}>{this.state.checkBoxStatus}要传的值：{this.state.toSonCheckBoxStatus}</a>
+                        {/*<a className="slide-tree-checkbox checkBoxBg checkBoxBg_1"></a>*/}
                     </span>
                      <i onClick={this.branchToggle} className={this.state.special=='all'?'icon-all': !this.state.isFolder?'icon-file': this.state.isFolder&&this.state.open?'icon-open': this.state.isFolder&&!this.state.open?'icon-folder':''}></i>
                      <span className="treeModelName">{this.state.modelItem.projectName}</span>
                  </div>
              </div>
+                 { this.state.isFolder? <div  style={{display :this.state.open?'block':'none'}}><TreeElement model={this.state.modelChild} sendCheckStatus={this.state.toSonCheckBoxStatus} /> </div>:null  }
+             {/*{ this.state.isFolder&&this.state.open? <TreeElement model={this.state.modelChild}  />:null  }*/}
 
-                 { this.state.isFolder? <div  style={{display :this.state.open?'block':'none'}}>
-                         {
-                             this.state.modelChild.map(function (number,index){
-                                 return (
-                                     <ul className="slide-tree" key={number.projectId}>
-                                         <TreeItemElement model={number} index={index} commitCheckState = {_this.commitCheckState.bind(this)} />
-                                     </ul>
-                                 )
-                             })
-                         }
-                     </div>   :null  }
          </li>
      )
     }
@@ -229,22 +170,17 @@ class Tree extends Component {
         }
     }
     componentDidMount(nextProps){
-        var modelChild=$getTreeList()
-        for(var i in modelChild){
-            modelChild[i]['checkState']=0
-        }
         this.setState({
-            model:modelChild
+            model:$getTreeList()
         })
     }
+    componentWillUnmount(){
 
-
+    }
     render() {
         return (
-        <div className="">
-            <h2>简单的Tree实现，在测试CheckBox选项前，先多展开几个子项</h2>
-            <TreeElement model={this.state.model}  />
-        </div>
+        <div className="slide-tree-edit"> <TreeElement model={this.state.model} /></div>
+
         );
     }
 }
